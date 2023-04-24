318d28d79691b82ae11de2b15d7beda051f87300
Ousmane96 <67338991+Ousmane96@users.noreply.github.com>
2023-04-24 08:00:30 +0200

    Update lufthansa.py

--- lufthansa.py
+++ lufthansa.py
@@ -8,47 +8,39 @@
 import pandas as pd
 import requests
 from time import sleep
+import datetime
+import itertools
+from tqdm import tqdm
+
 
 
 
 #
 def get_country (Access_Token) :
     
-    """This function takes as parameter the API access token provided by lufthansa "LH OpenAPI testing" and returns a DataFrame of the country. """
+    """This function takes as parameter the API access token provided \
+by lufthansa "LH OpenAPI testing" and returns a DataFrame of the country. """
     
     # 
     authentification = {}
     authentification["Authorization"] = "Bearer " + Access_Token
 
     #
-    country_endpoints = [] 
-    response_list = []
-    CountryCode = []
-    CountryName = []
     
+    df_list = []
+    offset_list = list(range(0,300,100))
+
+
+    for i in offset_list:
+        response = requests.get(("https://api.lufthansa.com/v1/mds-references/countries/?lang=AN&limit=100&offset={offset}").format(offset = i), headers = authentification).json()
+        df = pd.DataFrame(response['CountryResource']['Countries']['Country'])
+        df_list.append(df)
+        Countries = pd.concat(df_list)
+        Countries["Names"] = Countries["Names"].apply(lambda x : x['Name'][ '$'])
+        Countries = Countries.drop_duplicates().reset_index(drop = True)
+        #Countries.insert(0, "CountryId" , Countries.index+1)
     
-    #   
-    response = requests.get("https://api.lufthansa.com/v1/mds-references/countries/?lang=AN&limit=100&offset=0", headers = authentification).json()
-    for i in range(len(response['CountryResource']['Meta']['Link'])-1):  
-        country_endpoints.append(response['CountryResource']['Meta']['Link'][i]['@Href'])
-
-    #
-    for endpoint in country_endpoints: 
-        response_list.append(requests.get(endpoint , headers = authentification).json()['CountryResource']['Countries']['Country'])
-
-    for response in response_list:
-        for i in response:
-            CountryCode.append(i['CountryCode'])
-            CountryName.append(i['Names']['Name']['$'])
-    
-    #
-    Countries = pd.DataFrame({
-        "CountryCode" : CountryCode,
-        "CountryName" : CountryName
-    })
-
     return Countries
-
 
 
 #
@@ -57,35 +49,21 @@ def get_cities(Access_Token)  :
     #
     authentification = {}
     authentification["Authorization"] = "Bearer " + Access_Token
-
-    #
+    
     df_list = []
     offset_list = list(range(0,10700,100))
-
     #
     for i in offset_list:
-        sleep(0.1)
+        sleep(0.09)
         response = requests.get(("https://api.lufthansa.com/v1/mds-references/cities/?lang=AN&limit=100&offset={offset}").format(offset = i), headers = authentification).json()
-        pd.DataFrame(response['CityResource']['Cities']['City'])
         df_list.append(pd.DataFrame(response['CityResource']['Cities']['City']))
-
-    #
-    for df in df_list :
-        for i in range(len(df)):
-            city_name = df.loc[i, "Names"]["Name"]["$"]
-            df.loc[i, "Names"] = city_name
-        #    
-        for i in range(len(df)):
-            if isinstance(df.loc[i, "Airports"], dict) == True:       
-                airport_code = df.loc[i, "Airports"]["AirportCode"]
-                df.loc[i, "Airports"] = airport_code
-
-    Cities = pd.concat(df_list) 
-    Cities = Cities.explode("Airports")
-    Cities = Cities.reset_index(drop=True)
-    
+        Cities = pd.concat(df_list).reset_index(drop = True)
+        Cities["Names"] = Cities["Names"].apply(lambda x : x['Name'][ '$'])
+        Cities["Airports"] = Cities["Airports"].apply(lambda x: x['AirportCode'] if isinstance(x, dict) else None)
+        
+    Cities = Cities.explode("Airports").drop_duplicates().reset_index(drop=True)
+    Cities.insert(0, "CountryId" , Cities.index+1)
     return Cities
-
 
 
 #
@@ -98,6 +76,7 @@ def get_airports (Access_Token)  :
     #
     df_list = []
     offset_list = list(range(0,1500,100))
+    offset_list[offset_list.index(100)]= 170
     offset_list[offset_list.index(1000)]= 1005
     
     for i in offset_list:
@@ -150,3 +129,71 @@ def get_aircrafts(Access_Token) :
 
     Aircrafts = df
     return Aircrafts
+
+
+def get_flight_schedules(Access_Token, origins, destinations, start_date, num_days):
+
+    authentification = {}
+    authentification["Authorization"] = "Bearer " + Access_Token
+
+
+    dates = [start_date + datetime.timedelta(days=x) for x in range(num_days)]
+    dates = [d.strftime('%Y-%m-%d') for d in dates]
+    
+    df_list = []
+    counter = 0
+    
+    for origin, destination, date in tqdm(itertools.product(origins, destinations, dates)):
+        if origin == destination:
+            continue
+
+        try:
+            response = requests.get(
+                ("https://api.lufthansa.com/v1/operations/schedules/{origin}/{destination}/{date}"
+                 "?directFlights=0&limit=100&offset=0"
+                ).format(origin = origin, destination = destination, date = date),
+                headers=authentification).json()
+
+            for element in response['ScheduleResource']['Schedule']:
+                for flight in element.get('Flight', []):
+                    departure = flight.get('Departure', {})
+                    arrival = flight.get('Arrival', {})
+                    carrier = flight.get('MarketingCarrier', {})
+                    equipment = flight.get('Equipment', {})
+                    details = flight.get('Details', {})
+
+                    df = pd.DataFrame({
+                        'DepAirportCode': departure.get('AirportCode'),
+                        'DepScheduledTimeLocal': departure.get('ScheduledTimeLocal', {}).get('DateTime'),
+                        'ArrAirportCode': arrival.get('AirportCode'),
+                        'ArrScheduledTimeLocal': arrival.get('ScheduledTimeLocal', {}).get('DateTime'),
+                        'Terminal': arrival.get('Terminal', {}).get('Name'),
+                        'AirlineID': carrier.get('AirlineID'),
+                        'FlightNumber': carrier.get('FlightNumber'),
+                        'AircraftCode': equipment.get('AircraftCode'),
+                        'DaysOfOperation' : details.get('DaysOfOperation',),
+                        'Effective' : details.get('DatePeriod',).get('Effective'),
+                        'Expiration' : details.get('DatePeriod',).get('Expiration')                  
+                    }, index=[0])
+
+                    df_list.append( df[df['AirlineID'] == "LH"].reset_index(drop=True))
+
+            counter += 1
+
+            # Pause de 0,2 seconde toutes les 5 requetes
+            if counter % 5 == 0:
+                time.sleep(0.2)
+
+                
+
+            # Pause de 50 nimutes toutes les 1000 requetes
+            #if counter % 1000 == 0:
+                #time.sleep(3000)
+                #FlightSchedules.to_csv(f"/content/drive/MyDrive/europe_europe{counter}.csv", index=False)
+
+        except:
+            continue
+
+    FlightSchedules = pd.concat(df_list).drop_duplicates().reset_index(drop=True)
+    FlightSchedules.insert(0, "FlightScheduleID" , FlightSchedules.index+1)
+    return FlightSchedules
